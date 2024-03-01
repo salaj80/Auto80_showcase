b85cb7c995bbaed551bfd89b950f22f23518294e
Dimitar Lazarov <117237901+salaj80@users.noreply.github.com>
2024-03-01 02:29:17 +0100

    handle scroll fix

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,9 +5,13 @@ import CustomButton from "./CustomButton"
 
 const Hero = () => {
 
-    const handleScroll = () => {
+  const handleScroll = () => {
+    const nextSection = document.getElementById("discover");
 
+    if (nextSection) {
+      nextSection.scrollIntoView({ behavior: "smooth" });
     }
+  };
 
   return (
     <div className="hero">
