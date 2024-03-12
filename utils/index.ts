f3e230d7f192a33b1b636041b88073fc8c61735c
Dimitar Lazarov <117237901+salaj80@users.noreply.github.com>
2024-03-12 03:07:15 +0100

    api fix

--- utils/index.ts
+++ utils/index.ts
@@ -22,11 +22,9 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
     const mileageFactor = 0.1; 
     const ageFactor = 0.05; 
   
-
     const mileageRate = city_mpg * mileageFactor;
     const ageRate = (new Date().getFullYear() - year) * ageFactor;
   
-
     const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
   
     return rentalRatePerDay.toFixed(0);
